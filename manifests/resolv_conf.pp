3dc91fd97c22cc3b11b52d4c348a9b1cfdf1b7e4
habbis <habbis@habbestad.org>
2022-01-24 22:52:25 +0100

    added class resolv_conf to manage resolv.conf

---
+++ manifests/resolv_conf.pp
@@ -0,0 +1,17 @@
+class puppet_homelab::resolv_conf {
+
+
+exec {
+    'disable_esystemd_resolv':
+     path        => ['/bin'],
+     command     => 'systemctl disable --now systemd-resolved.service',
+  }
+
+
+file { '/etc/resolv.conf':
+     owner  => root,
+     group  => root,
+     mode   => '0644',
+     source => 'puppet:///modules/puppet_homelab/resolv_conf/resolv.conf'
+  }
+}
