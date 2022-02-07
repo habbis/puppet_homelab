65c092d18486821e7ac9c859aa49abe6c485503a
habbis <habbis@habbestad.org>
2022-02-07 01:07:13 +0100

    fixing gitea class

--- manifests/gitea.pp
+++ manifests/gitea.pp
@@ -1,25 +1,19 @@
 # Class for installing gitea on linux
 class puppet_homelab::gitea ($gitea_version) {
 
-  #package { 
-  #  'wget':  ensure => installed;
-  #  'git':  ensure => installed;
-  #  }
 
   exec {
     'gitea_download':
-     #path        => ['/bin'],
-     command     => '/usr/bin/wget -O gitea http://mirror.habbfarm.net/repository/binary_and_tar_files/gitea/gitea-1.16.0-linux-amd64 ; /usr/bin/mv gitea /usr/local/bin/',
-     #command     => '/usr/bin/wget https://dl.gitea.io/gitea/$gitea_version/gitea-$gitea_version-linux-amd64 ; /bin/cp gitea-$gitea_version-linux-amd64 /usr/local/bin/gitea',
-     unless      => '/usr/bin/test -e /usr/local/bin/gitea',
+     path        => ['/bin','/usr/bin', '/usr/sbin'],
+     command     => 'wget -O gitea http://mirror.habbfarm.net/repository/binary_and_tar_files/gitea/gitea-1.16.0-linux-amd64 ; mv gitea /usr/local/bin/',
+     unless      => 'test -e /usr/local/bin/gitea',
    }
 
 
   exec {
     'gitea_chmod_executable':
-     #path        => ['/bin'],
-     command     => '/usr/bin/chmod +x /usr/local/bin/gitea',
-     #unless      => '/usr/bin/test -e /usr/local/bin/gitea',
+     path        => ['/bin','/usr/bin', '/usr/sbin'],
+     command     => 'chmod +x /usr/local/bin/gitea',
    }
 
   user { 'git':
@@ -88,18 +82,11 @@ class puppet_homelab::gitea ($gitea_version) {
 
   exec {
     'gitea_enable_start':
-     path        => ['/bin'],
+     path        => ['/bin','/usr/bin', '/usr/sbin'],
      command     => 'systemctl enable --now gitea',
      unless      => ['test -e /etc/systemd/system/gitea.service'],
      #refreshonly => true;
   }
-
-#  exec {
-#   'gitea_restart':
-#    path        => ['/bin'],
-#    command     => 'systemctl start gitea',
-#    refreshonly => true;
-# }
 
 
 }
