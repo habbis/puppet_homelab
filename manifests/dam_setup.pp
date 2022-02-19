fde991f235c036ce1e3f3862862f76fc0dcfe4a3
habbis <habbis@medmail.ch>
2022-02-19 17:19:31 +0100

    removing dam_packages.pp from dam_setup for now

--- manifests/dam_setup.pp
+++ manifests/dam_setup.pp
@@ -1,6 +1,6 @@
 # setup dam server
 class puppet_homelab::dam_setup {
 
-  include puppet_homelab::dam_setup::dam_packages
+  #include puppet_homelab::dam_setup::dam_packages
   include puppet_homelab::dam_setup::dam_user
 }
