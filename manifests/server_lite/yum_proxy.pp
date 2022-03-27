ce4d96a6d069d3605b82faa25fe84a5d6696fd98
habbis <habbis@medmail.ch>
2022-03-27 23:08:27 +0200

    testing fix for class yum_repo.pp

--- manifests/server_lite/yum_proxy.pp
+++ manifests/server_lite/yum_proxy.pp
@@ -1,13 +1,18 @@
 # Class to add yum-proxy if the operatingsystem supports it.
 class puppet_homelab::server_lite::yum_proxy {
   #case $::operatingsystem {
-  case $facts['os']['name'] {
+  #case $facts['os']['name'] {
     #/^(RedHat|CentOS|Rocky)$/:{ include puppet_homelab::yum_proxy::config }
     #/^(Debian|Ubuntu)$/:{ }
-    'RedHat','CentOS','Rocky':{ include puppet_homelab::yum_proxy::config }
-    /^(Debian|Ubuntu)$/:{ }
-    default: {
-      fail("Module ${module_name} is not supported on ${::operatingsystem}")
-    }
-  }
+    #'RedHat','CentOS','Rocky':{ include puppet_homelab::yum_proxy::config }
+    #/^(Debian|Ubuntu)$/:{ }
+    #default: {
+    #  fail("Module ${module_name} is not supported on ${::operatingsystem}")
+    #}
+  #}
+if $::osfamily == 'RedHat' {
+    include puppet_homelab::yum_proxy::config
+  #} else {
+  #  $nfs_client = 'nfs-common'
+  #}
 }
