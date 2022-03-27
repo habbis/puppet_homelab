88cc617bbca6eb734ef2aaebd730eb69bcff9414
Eirik Habbestad Helleve <habbis@habbestad.org>
2022-03-27 22:28:28 +0200

    Update yum_proxy.pp

--- manifests/server_lite/yum_proxy.pp
+++ manifests/server_lite/yum_proxy.pp
@@ -1,8 +1,7 @@
 # Class to add yum-proxy if the operatingsystem supports it.
 class puppet_homelab::server_lite::yum_proxy {
   case $::operatingsystem {
-    /^(RedHat)$/:{ include puppet_homelab::yum_proxy::config }
-    /^(Rocky)$/:{ include puppet_homelab::yum_proxy::config }
+    /^(RedHat|Rocky)$/:{ include puppet_homelab::yum_proxy::config }
     /^(Debian|Ubuntu)$/:{ }
     default: {
       fail("Module ${module_name} is not supported on ${::operatingsystem}")
